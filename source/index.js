163ceba5627d850a40dfa0431a6ee54ff4d1d138
slavamak <slavamak.dev@gmail.com>
2020-08-27 21:33:58 +0300

    Homework for Lesson-5

--- source/index.js
+++ source/index.js
@@ -50,12 +50,14 @@ function init() {
   character.renderHP();
   enemy.renderHP()
 
-  console.log('Start Game!');
+  renderLog('Start Game!');
 };
 
 function renderHP() {
-  this.elHP.innerText = this.hp.damage + ' / ' + this.hp.default;
-  this.elProgressbar.style.width =  this.hp.damage / this.hp.default * 100 + '%';
+  const {elHP, elProgressbar, hp: {default: base, damage}} = this;
+
+  elHP.innerText = damage + ' / ' + base;
+  elProgressbar.style.width =  damage / base * 100 + '%';
 };
 
 function renderTechniques(array) {
@@ -81,6 +83,9 @@ function setDamage(e) {
 function changeHP(count = 10) {
   this.hp.damage -= count;
 
+  const message = this === enemy ? getLog(enemy, character, count) : getLog(character, enemy, count);
+  renderLog(message);
+
   if (this.hp.damage <= 0) {
     this.hp.damage = 0;
     gameOver.apply(this);
@@ -100,7 +105,35 @@ function gameOver() {
     $buttons[i].disabled = true;
   };
 
-  alert(this.name + ' lost!');
+  renderLog(this.name + ' lost!');
 };
+
+function renderLog(string) {
+  const logs = getNode('#logs');
+  const p = document.createElement('p');
+  p.innerText = '###: ' + string;
+
+  logs.insertBefore(p, logs.children[0]);
+};
+
+function getLog(firstPokemon, secondPokemon, count) {
+  const logs = [
+    `${firstPokemon.name} вспомнил что-то важное, но неожиданно ${secondPokemon.name}, не помня себя от испуга, ударил в предплечье врага.`,
+    `${firstPokemon.name} поперхнулся, и за это ${secondPokemon.name} с испугу приложил прямой удар коленом в лоб врага.`,
+    `${firstPokemon.name} забылся, но в это время наглый ${secondPokemon.name}, приняв волевое решение, неслышно подойдя сзади, ударил.`,
+    `${firstPokemon.name} пришел в себя, но неожиданно ${secondPokemon.name} случайно нанес мощнейший удар.`,
+    `${firstPokemon.name} поперхнулся, но в это время ${secondPokemon.name} нехотя раздробил кулаком \<вырезанно цензурой\> противника.`,
+    `${firstPokemon.name} удивился, а ${secondPokemon.name} пошатнувшись влепил подлый удар.`,
+    `${firstPokemon.name} высморкался, но неожиданно ${secondPokemon.name} провел дробящий удар.`,
+    `${firstPokemon.name} пошатнулся, и внезапно наглый ${secondPokemon.name} беспричинно ударил в ногу противника.`,
+    `${firstPokemon.name} расстроился, как вдруг, неожиданно ${secondPokemon.name} случайно влепил стопой в живот соперника.`,
+    `${firstPokemon.name} пытался что-то сказать, но вдруг, неожиданно ${secondPokemon.name} со скуки, разбил бровь сопернику.`
+  ];
+
+  const info = ` -${count} [${firstPokemon.hp.damage}/${firstPokemon.hp.default}]`;
+  const log =  logs[random(logs.length - 1)] + info;
+
+  return log;
+}
 
 init();
